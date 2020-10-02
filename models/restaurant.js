dbc2c5851768f637852742275049f4677df94592
lex-cool <66706246+lex-cool@users.noreply.github.com>
2020-10-02 16:28:04 -0400

    add average display

--- models/restaurant.js
+++ models/restaurant.js
@@ -14,10 +14,11 @@ const safetyReviewSchema = new Schema({
     staffWearsMasks: Boolean,
     staffWearsGloves: Boolean,
     staffGetsTemperatureChecks: Boolean,
+    masksRequired: Boolean,
     handSanitizerProvided: Boolean,
     customersGetTemperatureChecks: Boolean,
     safetyRating: {type: Number, default: null},
-    author: {type: Schema.Types.ObjectId, ref: 'User'}
+    author: {type: Schema.Types.ObjectId, ref: 'User'},
 })
 
 
@@ -30,7 +31,13 @@ const restaurantSchema = new Schema({
     address: String,
     cuisine: [Object],
     price: String,
-    safetyReviews: {type: [safetyReviewSchema], default: []}
+    safetyReviews: {type: [safetyReviewSchema], default: []},
+    avMasksReq: {type: Boolean, default: false},
+    avStaffMasks: {type: Boolean, default: false},
+    avSocDist: {type: Boolean, default: false},
+    avLimCap: {type: Boolean, default: false},
+    avSanBet: {type: Boolean, default: true},
+    avCLessPay: {type: Boolean, default: false}
 }, {timestamps: true})
 
 restaurantSchema.methods.getAverageSafetyRating = function() {
@@ -40,6 +47,33 @@ restaurantSchema.methods.getAverageSafetyRating = function() {
     }, 0)
     averageRating = total / this.safetyReviews.filter(review => review.safetyRating != null).length
     return averageRating
+}
+
+restaurantSchema.methods.setAverages = function() {
+    this.avMasksReq = this.safetyReviews.reduce((bools, rev) => {
+        if (rev.masksRequired) bools += 1
+        return bools
+    }, 0) > 0.5 * this.safetyReviews.length ? true : false
+    this.avStaffMasks = this.safetyReviews.reduce((bools, rev) => {
+        if (rev.staffWearsMasks) bools += 1
+        return bools
+    }, 0) > 0.5 * this.safetyReviews.length ? true : false
+    this.avSocDist = this.safetyReviews.reduce((bools, rev) => {
+        if (rev.socialDistancingEnforced) bools += 1
+        return bools
+    }, 0) > 0.5 * this.safetyReviews.length ? true : false
+    this.avLimCap = this.safetyReviews.reduce((bools, rev) => {
+        if (rev.limitedCapacity) bools += 1
+        return bools
+    }, 0) > 0.5 * this.safetyReviews.length ? true : false
+    this.avSanBet = this.safetyReviews.reduce((bools, rev) => {
+        if (rev.staffSanitizeHandsBetCustomers) bools += 1
+        return bools
+    }, 0) > 0.5 * this.safetyReviews.length ? true : false
+    this.avCLessPay = this.safetyReviews.reduce((bools, rev) => {
+        if (rev.contactlessPayment) bools += 1
+        return bools
+    }, 0) > 0.5 * this.safetyReviews.length ? true : false
 }
 
 module.exports = mongoose.model('Restaurant', restaurantSchema)
